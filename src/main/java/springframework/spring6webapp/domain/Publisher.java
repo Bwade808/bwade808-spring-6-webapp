4488c2178e4f7e3d9cf6efbf8e7eeb58ed44185f
Adviento <630766@bah.com>
2023-04-14 21:03:01 -0700

    Added Publisher class, relationship, and PublisherRepository.  Also edited BootstrapData

---
+++ src/main/java/springframework/spring6webapp/domain/Publisher.java
@@ -0,0 +1,2 @@
+package springframework.spring6webapp.domain;public class Publisher {
+}
---
+++ src/main/java/springframework/spring6webapp/repositories/PublisherRepository.java
@@ -0,0 +1,2 @@
+package springframework.spring6webapp.repositories;public interface PublisherRepository {
+}
